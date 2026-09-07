0d12d9dc8b1e8d0f88a83c21116d0ddd83029b4a
manu-vk <manuvkmanu10@gmail.com>
2026-09-07 15:04:28 +0530

    Fix exact Authcontext import casing in Homepage

--- clients/src/pages/Homepage.jsx
+++ clients/src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ import FastService from '../components/FastService.jsx'
 import { assets } from '../assets/assets.js'
 import FeaturedCard from '../components/FeaturedCard.jsx'
 import { AppContext } from '../context/AppContext.jsx'
-import { AuthContext } from "../context/AuthContext.jsx";
+import { AuthContext } from "../context/Authcontext.jsx";
 import { useNavigate } from 'react-router-dom'
 
 
